dcbaab3dcc70692f6caada3da35e41db210b2861
Mark Boon <mark_boon@me.com>
2012-01-20 02:55:18 -1000

    Transfer script

--- update_bot.sh
+++ update_bot.sh
@@ -1,4 +1,4 @@
-cp Documents/workspace/BotL/*.botl transfer
-cp Documents/workspace/BotL/BotServer.jar transfer
-cp Documents/workspace/BotL/botl.server.war transfer
+cp *.botl transfer
+cp BotServer.jar transfer
+cp BotL/botl.server.war transfer
 rsync -avPe 'ssh -i tetris_stars.pem' transfer ubuntu@ec2-107-21-174-126.compute-1.amazonaws.com:~/
